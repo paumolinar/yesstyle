021b609684ed6ee588eb688fa43db1036f56cbb0
Paula Molina <pau.molina@duocuc.cl>
2024-12-11 19:57:35 -0300

    add media config, image field, install pillow

--- app/models.py
+++ app/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=100)
     brand = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='products/')
 
     def __str__(self):
         return self.name
