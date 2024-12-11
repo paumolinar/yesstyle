310c70f214be8c40d1a6a3cb38f6feb8004c8b88
Paula Molina <pau.molina@duocuc.cl>
2024-12-10 22:01:07 -0300

    add product model

--- app/views.py
+++ app/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from app import models
 
 def home(request):
-    return render(request, "home.html")
+    products = models.Product.objects.all()
+    context = {
+        'products': products
+    }
+    return render(request, "home.html", context)
